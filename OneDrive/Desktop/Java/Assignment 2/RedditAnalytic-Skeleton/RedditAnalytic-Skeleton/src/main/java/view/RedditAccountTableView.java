bfbc9d5b9e1e701a35157df2179975edff68e30d
alexiossu <boringalexi@gmail.com>
2020-12-13 19:10:25 -0500

    second commit

--- OneDrive/Desktop/Java/Assignment 2/RedditAnalytic-Skeleton/RedditAnalytic-Skeleton/src/main/java/view/RedditAccountTableView.java
+++ OneDrive/Desktop/Java/Assignment 2/RedditAnalytic-Skeleton/RedditAnalytic-Skeleton/src/main/java/view/RedditAccountTableView.java
@@ -1,6 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package view;
 
 import entity.Account;
+import entity.RedditAccount;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
@@ -13,60 +19,70 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import logic.AccountLogic;
 import logic.LogicFactory;
+import logic.RedditAccountLogic;
 
 /**
  *
- * @author Shariar (Shawn) Emami
+ * @author surko
  */
-@WebServlet( name = "AccountTable", urlPatterns = { "/AccountTable" } )
-public class AccountTableView extends HttpServlet {
+@WebServlet(name = "RedditAccountTableView", urlPatterns = {"/RedditAccountTableView"})
+public class RedditAccountTableView extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest( HttpServletRequest request, HttpServletResponse response )
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType( "text/html;charset=UTF-8" );
-        try( PrintWriter out = response.getWriter() ) {
-            out.println( "<!DOCTYPE html>" );
-            out.println( "<html>" );
-            out.println( "<head>" );
-            out.println( "<title>AccountViewNormal</title>" );
-            out.println( "</head>" );
-            out.println( "<body>" );
-
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>RedditAccountTableView</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
             out.println( "<table style=\"margin-left: auto; margin-right: auto;\" border=\"1\">" );
-            out.println( "<caption>Account</caption>" );
-            //this is an example, for your other tables use getColumnNames from
-            //logic to create headers in a loop.
-            out.println( "<tr>" );
-            out.println( "<th>ID</th>" );
-            out.println( "<th>Displayname</th>" );
-            out.println( "<th>Username</th>" );
-            out.println( "<th>Password</th>" );
-                    out.println( "</tr>" );
-
-            AccountLogic logic = LogicFactory.getFor( "Account" );
-            List<Account> entities = logic.getAll();
-            for( Account e: entities ) {
+            out.println( "<caption>RedditAccountTableView</caption>" );
+            
+            RedditAccountLogic logic = LogicFactory.getFor("RedditAccount");
+            
+            for (int i = 0; i < logic.getColumnNames().size(); i++) {
+                
+                out.println(logic.getColumnNames().get(i));
+            }
+            
+             out.println( "</tr>" );
+             
+              List<RedditAccount> entities = logic.getAll();
+            for( RedditAccount e: entities ) {
                 //for other tables replace the code bellow with
                 //extractDataAsList in a loop to fill the data.
-                out.printf( "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
-                        logic.extractDataAsList( e ).toArray() );
+              //  out.printf( "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
+                     //   logic.extractDataAsList( e ).toArray() );
+                     
+                     for (int i = 0; i < logic.extractDataAsList(e).size(); i++) {
+                         
+                         out.println(logic.extractDataAsList( e ).get(i));
+                     }
+                        
+                     out.println( "<tr>" );
+                     
             }
-
-            out.println( "<tr>" );
             //this is an example, for your other tables use getColumnNames from
             //logic to create headers in a loop.
-            out.println( "<th>ID</th>" );
-            out.println( "<th>Displayname</th>" );
-            out.println( "<th>Username</th>" );
-            out.println( "<th>Password</th>" );
+            for (int i = 0; i < logic.getColumnNames().size(); i++) {
+                
+               out.println(logic.getColumnNames().get(i));     
+            }
+            
             out.println( "</tr>" );
             out.println( "</table>" );
             out.printf( "<div style=\"text-align: center;\"><pre>%s</pre></div>", toStringMap( request.getParameterMap() ) );
@@ -74,7 +90,7 @@ public class AccountTableView extends HttpServlet {
             out.println( "</html>" );
         }
     }
-
+    
     private String toStringMap( Map<String, String[]> m ) {
         StringBuilder builder = new StringBuilder();
         for( String k: m.keySet() ) {
@@ -86,6 +102,7 @@ public class AccountTableView extends HttpServlet {
         return builder.toString();
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -94,39 +111,26 @@ public class AccountTableView extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
+   @Override
     protected void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
         log( "GET" );
         processRequest( request, response );
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
         log( "POST" );
-        AccountLogic logic = LogicFactory.getFor( "Account" );
+        AccountLogic logic = LogicFactory.getFor( "RedditAccount" );
         Account account = logic.updateEntity( request.getParameterMap() );
         logic.update( account );
         processRequest( request, response );
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
-        return "Sample of Account View Normal";
+        return "Sample of RedditAccount View Normal";
     }
 
     private static final boolean DEBUG = true;
